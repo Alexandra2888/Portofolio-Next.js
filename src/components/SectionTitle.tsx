7851966250af140a5b6fec3dacb7c0d32adc4233
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-08-28 14:55:39 +0300

    adding interface

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
-const SectionTitle  = ({text}) => {
+interface ISectionTitle {
+  text: string
+}
+
+const SectionTitle  = ({text}:ISectionTitle) => {
   return (
     <section className='border-b border-gray-200 pb-5'>
       <h2 className='text-3xl font-medium tracking-wider capitalize'>{text}</h2>
