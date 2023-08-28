a209cbb2524da634d82426737833009f1188fe05
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-08-28 14:51:25 +0300

    fix build

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ import SectionTitle from "@/components/SectionTitle";
   return (
    <section className='bg-white py-20' id='about'>
     <div className='mx-auto max-w-7xl px-8 grid md:grid-cols-2 items-center gap-16'>
-        <Image src="../../about.svg" width={16} height={16} className='w-full h-64'/>
+        <Image src="../../about.svg" alt='about' width={16} height={16} className='w-full h-64'/>
         <article>
             <SectionTitle text='code and coffee'/>
             <p className='text-slate-600 mt-8 leading-loose'>
